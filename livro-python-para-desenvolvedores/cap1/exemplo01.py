ac56c698c0199477b04a51966048ba10933dc8a6
Wellington-Junior <wndrj7@gmail.com>
2017-06-15 11:31:58 -0300

    Exemplos capitulo1

---
+++ livro-python-para-desenvolvedores/cap1/exemplo01.py
@@ -0,0 +1,7 @@
+#uma lista de instrumentos musicais
+instrumentos = ['Baixo','Bateria','Guitarra']
+
+#Para cada nome na lista de instrumentos
+for instrumento in instrumentos:
+    #mostre o nome do instrumento musical
+    print instrumento
---
+++ livro-python-para-desenvolvedores/cap1/exemplo02.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+#-*-coding: latin1 -*-
+
+# Uma linha de código que mostra o resultado de 7 vezes 3
+print 7 * 3
